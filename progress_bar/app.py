fc25e64b431abd0b009948426b93ba35a2c118a8
Sagar Shah <sagar.23723@gmail.com>
2024-11-28 13:52:19 -0400

    updated port

--- progress_bar/app.py
+++ progress_bar/app.py
@@ -157,4 +157,6 @@ def progress_bar():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    # Use PORT environment variable or default to 8001 if not set
+    port = int(os.environ.get("PORT", 8001))
+    app.run(host="0.0.0.0", port=port)
